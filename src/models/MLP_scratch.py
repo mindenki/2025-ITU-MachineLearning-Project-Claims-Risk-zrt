3f0391be238589f70da294384966a1b503b94de9
Etele Kovács <etikova777@gmail.com>
2025-11-17 14:27:45 +0100

    Added scratch_MLP to models branch

--- src/models/MLP_scratch.py
+++ src/models/MLP_scratch.py
@@ -2,7 +2,7 @@ from math import sqrt
 import numpy as np
 
 def ReLU(x):
-    return max(0, x)
+    return np.maximum(0, x)
 
 def dReLU(x):
     return (x>0.0).astype(x.dtype)
@@ -11,7 +11,7 @@ class Layer:
     def __init__(self, in_feats, out_feats, activation, dactivation):
         # He parameter initialization
         rng = np.random.default_rng(42)
-        std = sqrt(2.0/len(in_feats))
+        std = sqrt(2.0/in_feats)
         self.W = rng.normal(0.0, std, size=(out_feats, in_feats))
         self.b = np.zeros(out_feats, dtype=np.float32)
         
@@ -24,13 +24,13 @@ class Layer:
         
     def forward(self, x):
         self.x = x
-        self.y = np.matmul(x, self.W) + self.b
+        self.y = np.matmul(x, self.W.T) + self.b
         return self.act(self.y)
     
     def backward(self, grad_out):
         
         # derivative of loss wrt the activated output (post activation)
-        D = grad_out * self.dact(self.z)
+        D = grad_out * self.dact(self.y)
         
         # derivative of loss wrt the weights
         # summing over the batch
@@ -38,7 +38,7 @@ class Layer:
         
         # derivative of loss wrt the bias
         # summing over the batch
-        self.b += D.sum(axis=0)
+        self.db += D.sum(axis=0)
         
         # derivative of loss wrt the inputs
         grad_in = np.matmul(D, self.W)
@@ -52,16 +52,16 @@ class Layer:
     def zero_grad(self):
         # reseting the gradients for weigths and biases after batch
         self.dW.fill(0.0)
-        self.dn.fill(0.0)
+        self.db.fill(0.0)
     
 class MLP:
     def __init__(self, input_dim, hidden_sizes, lr):
         self.lr = lr
         output_dim = 1
-        sizes = [input_dim] + len(hidden_sizes) + [output_dim]
-        activations = [(ReLU(), dReLU())] * len(hidden_sizes) + [(lambda x:x, lambda x:np.ones_like(x))]
+        sizes = [input_dim] + list(hidden_sizes) + [output_dim]
+        activations = [(ReLU, dReLU)] * len(hidden_sizes) + [(lambda x:x, lambda x:np.ones_like(x))]
         self.layers = []
-        for i in range(len(hidden_sizes)):
+        for i in range(len(sizes) - 1):
             self.layers.append(Layer(sizes[i], sizes[i+1], activations[i][0], activations[i][1]))
             
     def forward(self, x):
@@ -85,10 +85,9 @@ class MLP:
         
         
 def MSE(preds, targets):
-    n = preds.shape[0]
     diff = preds - targets
-    loss = (diff **2)/n
-    grad = (2.0 * diff)/n
+    loss = (diff **2).mean()
+    grad = (2.0 * diff) / preds.shape[0]
     return loss, grad
 
 # Example of one pass
@@ -98,3 +97,17 @@ def MSE(preds, targets):
 # model.zero_grad()
 # model.backward(dyp)
 # model.step()
+
+
+def batch_iterator(X, y, batch_size, shuffle=True, seed=42):
+    N = X.shape[0]
+    idx = np.arange(N)
+    rng = np.random.default_rng(seed) if shuffle else None
+    if shuffle:
+        rng.shuffle(idx)
+    for start in range(0, N, batch_size):
+        end = start + batch_size
+        b = idx[start:end]
+        Xb = X[b]
+        yb = y[b].reshape(-1, 1)
+        yield Xb, yb
