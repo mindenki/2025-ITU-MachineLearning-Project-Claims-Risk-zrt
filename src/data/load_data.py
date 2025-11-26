9bdf09de2068eec633d535d864616ab969a8cdda
anisk <kadem.anis04@gmail.com>
2025-11-26 22:46:01 +0100

    fixed structure and also created processed test X and y

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,6 +1,8 @@
 import pandas as pd 
+import numpy as np
 import os
 from pathlib import Path
+from scipy import sparse
 
 ROOT = Path(__file__).resolve().parents[2]
 DATA_DIR = ROOT / "data"
@@ -9,15 +11,18 @@ PROC_DIR = DATA_DIR / "processed"
 
 
 
-def load_data(raw: bool=True) -> tuple[pd.DataFrame, pd.DataFrame]:
+def load_data(raw: bool=True, target: str=None) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
-    Loads the raw data from the data directory.
+    Loads the raw data from the data directory as pandas DataFrames.
+    If raw is False, loads the processed data, as numpy arrays and sparse matrices.
     Parameters:
     ----------
     data_dir : str
         The directory where the data is stored.
     raw : bool
         If True, loads the raw data. If False, loads the processed data.
+    target : str, optional
+        The target variable to load.
     Returns:
     -------
     train_df : pd.DataFrame
@@ -28,16 +33,37 @@ def load_data(raw: bool=True) -> tuple[pd.DataFrame, pd.DataFrame]:
     if raw:
         train_path = RAW_DIR / "claims_raw_train.csv"
         test_path  = RAW_DIR / "claims_raw_test.csv"
+        if not os.path.exists(train_path):
+            raise FileNotFoundError(f"Training data not found at {train_path}")
+        if not os.path.exists(test_path):
+            raise FileNotFoundError(f"Test data not found at {test_path}")
+        train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path)
+        return train_df, test_df
+        
     else:
-        train_path = PROC_DIR / "claims_processed_train.csv"
-        test_path = PROC_DIR / "claims_processed_train.csv"
-    if not os.path.exists(train_path):
-        raise FileNotFoundError(f"Training data not found at {train_path}")
-    if not os.path.exists(test_path):
-        raise FileNotFoundError(f"Test data not found at {test_path}")
-    train_df = pd.read_csv(train_path)
-    test_df = pd.read_csv(test_path)
-    return train_df, test_df
+        X_train_path = PROC_DIR / "X_train.npz"
+        X_test_path = PROC_DIR / "X_test.npz"
+        y_train_path = PROC_DIR / f"y_train_{target}.npy"
+        y_test_path = PROC_DIR / f"y_test_{target}.npy"
+        if not os.path.exists(X_train_path):
+            raise FileNotFoundError(f"Training data not found at {X_train_path}")
+        if not os.path.exists(X_test_path):
+            raise FileNotFoundError(f"Test data not found at {X_test_path}")
+        if not os.path.exists(y_test_path):
+            raise FileNotFoundError(f"Test data not found at {y_test_path}")
+        if not os.path.exists(y_train_path):
+            raise FileNotFoundError(f"Training data not found at {y_train_path}")
+        
+        X_train = sparse.load_npz(X_train_path)
+        X_test = sparse.load_npz(X_test_path)
+        y_train = np.load(y_train_path)
+        y_test = np.load(y_test_path)
+        
+        return (X_train, y_train), (X_test, y_test)
+        
+    
+    
 
 if  __name__ == "__main__":
     train_df, test_df = load_data(raw=True)
