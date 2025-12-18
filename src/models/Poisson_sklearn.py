8ae8c4ee40f626c5ecef41537b042df0588d70fc
Luca <lucm@itu.dk>
2025-12-18 17:25:32 +0100

    Changed the poisson model to negative binomial

--- src/models/Poisson_sklearn.py
+++ src/models/Poisson_sklearn.py
@@ -1,16 +1,17 @@
 from sklearn.linear_model import PoissonRegressor
 
 
-class poisson_model():
+class poisson_model:
 
     def __init__(self, alpha = None, max_iter = None):
-        self.model = PoissonRegressor(alpha, max_iter)
+
+        self.model = PoissonRegressor(alpha = alpha, max_iter = max_iter)
         pass
 
-    def poisson_model(self, x, y):
-        return self.model.fit(x, y)
+    def fit(self, x, y):
+        self.model_fit = self.model.fit(x, y)
 
     def predict(self, X_test):
-        return self.model.predict(X_test)
+        return self.model_fit.predict(X_test)
 
 
